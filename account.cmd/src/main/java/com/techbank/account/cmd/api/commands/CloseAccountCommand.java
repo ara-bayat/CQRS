726d3b8ee34768570b813b0388efd9b78fb53e07
alireza <ara.bayat@gmail.com>
2026-08-20 18:31:43 +0330

    second phase

--- account.cmd/src/main/java/com/techbank/account/cmd/api/commands/CloseAccountCommand.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/api/commands/CloseAccountCommand.java
@@ -1,10 +1,7 @@
 package com.techbank.account.cmd.api.commands;
 
-import com.techbank.account.common.dto.AccountType;
 import com.techbank.cqrs.core.commands.BaseCommand;
 import lombok.Data;
-
-import java.math.BigDecimal;
 
 @Data
 public class CloseAccountCommand extends BaseCommand {
--- account.cmd/src/main/java/com/techbank/account/cmd/api/commands/DepositAccountCommand.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/api/commands/DepositAccountCommand.java
@@ -1,6 +1,5 @@
 package com.techbank.account.cmd.api.commands;
 
-import com.techbank.account.common.dto.AccountType;
 import com.techbank.cqrs.core.commands.BaseCommand;
 import lombok.Data;
 
